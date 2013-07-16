3051f81b30feb36c28c7ffd0265cba65bbd6d133
Russell Reas <russell.reas@gmail.com>
2013-07-16 09:57:32 -0700

    Remove some duplication in tests

--- test/ring_test/session_test.clj
+++ test/ring_test/session_test.clj
@@ -5,34 +5,31 @@
             [ring.middleware.session.cookie :refer [cookie-store]]
             [ring.mock.request :refer [request]]))
 
-(defn handler [{session :session}]
+(defn handler [{session :session :as r}]
   (let [n (:n session 1)]
     {:body (str n) :session {:n (inc n)}}))
 
 (def app-memory (wrap-session handler))
 (def app-cookie (wrap-session handler {:store (cookie-store {:key "0987654321012345"})}))
+(def app-secure (wrap-session handler {:store (cookie-store {:key "abcdefghijklmnop" :secure true})}))
 
-(deftest session-carries-over-memory
+(defn session-carries-over [app]
   (let [resp
         (run-ring-app
-          app-memory
+          app
           (request :get "/")
           (request :get "/"))]
     (is (= "2" (:body resp)))))
 
-(deftest session-carries-over-cookie
-  (let [resp
-        (run-ring-app
-          app-cookie
-          (request :get "/")
-          (request :get "/"))]
-    (is (= "2" (:body resp)))))
+(deftest session-carries-over-memory (session-carries-over app-memory))
+(deftest session-carries-over-cookie (session-carries-over app-cookie))
+(deftest session-carries-over-secure (session-carries-over app-secure))
 
-(deftest session-gets-reset-memory
-  (run-ring-app app-memory (request :get "/"))
-  (is (= "1" (:body (run-ring-app app-memory (request :get "/"))))))
+(defn session-resets [app]
+  (run-ring-app app (request :get "/"))
+  (is (= "1" (:body (run-ring-app app (request :get "/"))))))
 
-(deftest session-gets-reset-cookie
-  (run-ring-app app-cookie (request :get "/"))
-  (is (= "1" (:body (run-ring-app app-cookie (request :get "/"))))))
+(deftest session-resets-memory (session-resets app-memory))
+(deftest session-resets-cookie (session-resets app-cookie))
+(deftest session-resets-secure (session-resets app-secure))
 
